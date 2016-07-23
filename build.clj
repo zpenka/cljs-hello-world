edf14d7d9586e3ea73e615c4e3c5165be1d79e8e
Zack Penka <zpenka@shutterstock.com>
2016-07-22 22:50:24 -0400

    Change build to watch

--- build.clj
+++ build.clj
@@ -1,4 +1,4 @@
 (require 'cljs.build.api)
-(cljs.build.api/build "src"
+(cljs.build.api/watch "src"
                       {:main 'cljs-first-project.core
                         :output-to "out/main.js"})
